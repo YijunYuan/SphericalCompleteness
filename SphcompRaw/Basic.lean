a23297f502707d246e6738479c26711b72ada2f9
Yijun Yuan <941201yuan@gmail.com>
2025-12-07 23:05:48 +0800

    update

--- SphcompRaw/Basic.lean
+++ SphcompRaw/Basic.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+import Mathlib.Topology.MetricSpace.Pseudo.Defs
+import Mathlib.Topology.UniformSpace.Cauchy
+
+open Metric
+class SphericallyCompleteSpace (α : Type*) [PseudoMetricSpace α] : Prop where
+  isSphericallyComplete : ∀ ⦃ci : ℕ → α⦄, ∀ ⦃ri : ℕ → ℝ⦄,
+    Antitone (fun i => ball (ci i) (ri i)) → (⋂ i, ball (ci i) (ri i)) ≠ ∅
+
+instance instCompleteOfSphericallyComplete (α : Type*) [PseudoMetricSpace α] [SphericallyCompleteSpace α] : CompleteSpace α where
+  complete := by
+
+    sorry
