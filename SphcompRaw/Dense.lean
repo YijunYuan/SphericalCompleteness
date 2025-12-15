7ac780a96c6b9aa1b7e1fe30b56d56cb71f2bee4
Yijun Yuan <941201yuan@gmail.com>
2025-12-15 23:01:28 +0800

    fix: rename closure_eq_icc_iff lemma to icc_subset_closure_iff and update its logic

--- SphcompRaw/Dense.lean
+++ SphcompRaw/Dense.lean
@@ -5,15 +5,15 @@ import Mathlib.Tactic
 import Mathlib.Topology.Algebra.Valued.NormedValued
 import Mathlib.Analysis.Normed.Module.Basic
 import Mathlib.NumberTheory.Padics.ProperSpace
-
+import Mathlib.NumberTheory.Padics.Complex
 open Metric
 open Filter
 open TopologicalSpace
 open NNReal
 
-lemma closure_eq_icc_iff {a b : ℝ} (hab : a < b) (S : Set ℝ) :
+lemma icc_subset_closure_iff {a b : ℝ} (hab : a < b) (S : Set ℝ) :
   Set.Icc a b ⊆ closure S  ↔
-    ∀ a' b' : ℝ, a' < b' → Set.Icc a' b' ⊆ Set.Icc a b → ∃ s ∈ S, s ∈ Set.Icc a' b' := by
+    ∀ a' b' : ℝ, a' < b' → Set.Icc a' b' ⊆ Set.Icc a b → ∃ s ∈ S, s ∈ Set.Ioo a' b' := by
   constructor
   · intro h a' b' ha'b' hi
     by_contra hc
@@ -30,7 +30,9 @@ lemma closure_eq_icc_iff {a b : ℝ} (hab : a < b) (S : Set ℝ) :
       rw [Set.Icc_subset_Icc_iff]
       · exact ⟨by linarith, by linarith⟩
       · linarith
-    exact False.elim <| hc s hs <| this <| Set.mem_Icc_iff_abs_le.1 <| le_of_lt hs'
+    simp only [abs_lt, neg_lt_sub_iff_lt_add, c] at hs'
+    obtain ⟨hs'1, hs'2⟩ := hs'
+    exact False.elim <| hc s hs <| Set.mem_Ioo.1 ⟨by linarith, by linarith⟩
   · intro h z hz
     rw [Real.mem_closure_iff]
     intro ε hε
@@ -45,39 +47,38 @@ lemma closure_eq_icc_iff {a b : ℝ} (hab : a < b) (S : Set ℝ) :
       )
       rcases h with ⟨w, hw1, hw2⟩
       use w
-      rw [Set.mem_Icc, ←hh] at hw2
+      rw [Set.mem_Ioo, ←hh] at hw2
       refine ⟨hw1, abs_sub_lt_iff.mpr ⟨?_, by linarith⟩⟩
       replace hw2 := hw2.2
-      rw [add_comm, ← sub_le_iff_le_add] at hw2
-      exact lt_of_le_of_lt hw2 <|
+      rw [add_comm, ← sub_lt_iff_lt_add] at hw2
+      exact lt_trans hw2 <|
         lt_of_le_of_lt (min_le_left (ε / 2) (b - z)) (div_two_lt_of_pos hε)
     else
-    specialize h (z - min (ε / 2) (z - a)) z (by
+    specialize h (z - min (ε / 2) ((z - a) / 2)) z (by
       simp only [sub_lt_self_iff, lt_inf_iff, hε, div_pos_iff_of_pos_left, Nat.ofNat_pos, sub_pos,
         lt_of_le_of_ne hz.out.1 (Ne.symm hh), and_self]
       ) (by
       refine Set.Icc_subset_Icc ?_ hz.out.2
       rw [le_sub_iff_add_le, add_comm, ← le_sub_iff_add_le]
-      exact min_le_right (ε / 2) (z - a)
+      refine le_of_lt <| lt_of_le_of_lt (min_le_right (ε / 2) ((z - a) / 2)) ?_
+      simp only [half_lt_self_iff, sub_pos, lt_of_le_of_ne hz.out.1 (Ne.symm hh)]
       )
     rcases h with ⟨w, hw1, hw2⟩
     use w
     refine ⟨hw1, ?_⟩
     rw [abs_sub_comm]
     refine abs_sub_lt_iff.mpr ?_
-    simp only [Set.mem_Icc, tsub_le_iff_right] at hw2
-    constructor
-    · rw [sub_lt_iff_lt_add]
-      refine lt_of_le_of_lt hw2.1 ?_
-      nth_rw 1 [add_comm]
-      simp only [add_lt_add_iff_right, inf_lt_iff, half_lt_self_iff, hε, true_or]
-    · linarith
+    simp only [Set.mem_Ioo] at hw2
+    rw [sub_lt_iff_lt_add',← sub_lt_iff_lt_add] at hw2
+    exact ⟨lt_trans hw2.1 <| lt_of_le_of_lt (min_le_left _ _) <| div_two_lt_of_pos hε, by linarith⟩
 
 class DensePseudoMetricSpace (α : Type*) extends PseudoMetricSpace α where
   is_dense : ∀ z : α, ∀ ε : ℝ≥0,
   closure (Set.image2 dist (closedBall z ε) (closedBall z ε)) = Set.Icc 0 (diam (closedBall z ε))
 
-theorem test {α : Type*} [PseudoMetricSpace α] [hiud : IsUltrametricDist α] (z : α) (r : ℝ≥0) :
+theorem diam_le_radius_of_ultrametric {α : Type*}
+[PseudoMetricSpace α] [hiud : IsUltrametricDist α]
+{z : α} {r : ℝ≥0} :
 diam (closedBall z r) ≤ r := by
   apply diam_le_of_forall_dist_le
   · exact r.prop
@@ -87,26 +88,47 @@ diam (closedBall z r) ≤ r := by
     have := hiud.dist_triangle_max x z y
     grind only [= max_def]
 
-theorem test' {α : Type*} [PseudoMetricSpace α] [hiud : IsUltrametricDist α] (z : α) (r : ℝ≥0)
-(h : ∃ z' : α, dist z z' = r) :
+theorem diam_eq_radius_of_dense_ultrametric {α : Type*}
+[dnf : DenselyNormedField α] [hiud : IsUltrametricDist α]
+{z : α} {r : ℝ≥0} :
 diam (closedBall z r) = r := by
-  refine eq_of_le_of_ge (test _ _) ?_
-  rw [← h.choose_spec]
-  refine dist_le_diam_of_mem isBounded_closedBall ?_ ?_
-  · simp only [mem_closedBall, dist_self, dist_nonneg]
-  · simp only [mem_closedBall, dist_comm, le_refl]
+  refine eq_of_le_of_ge diam_le_radius_of_ultrametric ?_
+  by_contra hc
+  simp only [not_le] at hc
+  rcases dnf.lt_norm_lt (diam (closedBall z ↑r)) ↑r diam_nonneg hc with ⟨δ, _, hδ2⟩
+  have : z + δ ∈ closedBall z r := by
+    simp only [mem_closedBall, dist_self_add_left, le_of_lt hδ2]
+  have this' : z ∈ closedBall z r := by
+    simp only [mem_closedBall, dist_self, zero_le_coe]
+  have := dist_le_diam_of_mem isBounded_closedBall this this'
+  simp only [dist_self_add_left] at this
+  linarith
 
-lemma noname {α : Type*} [hdpm : DensePseudoMetricSpace α] [hiud : IsUltrametricDist α]
+lemma test {α : Type*}
+[dnf : DenselyNormedField α] [hiud : IsUltrametricDist α]
+{z : α} {r : ℝ≥0} (r' : ℝ≥0) (hr : r' < r) :
+∃ x y : α, x ∈ closedBall z r ∧ y ∈ closedBall z r ∧  nndist x y ∈ Set.Ioo r' r := by
+  rcases dnf.lt_norm_lt r' r r'.prop hr with ⟨δ, hδ1, hδ2⟩
+  use z + δ, z
+  refine ⟨?_,⟨?_,?_⟩⟩
+  · simp only [mem_closedBall, dist_self_add_left, le_of_lt hδ2]
+  · simp only [mem_closedBall, dist_self, zero_le_coe]
+  · unfold nndist PseudoMetricSpace.toNNDist
+    simp only [dist_self_add_left, Set.mem_Ioo]
+    exact ⟨hδ1, hδ2⟩
+
+lemma noname {α : Type*} [dnf : DenselyNormedField α] [hiud : IsUltrametricDist α]
 -- Ball B(c,r) with positive diameter d
-(c : α) (r : ℝ) (hr : r > 0)
-(d : ℝ) (hd : d > 0) (hdi : diam (closedBall c r) = d)
+(c₀ : α) (r₀ : ℝ≥0)
 --
-(d' : ℝ) (hd' : d' ∈ Set.Ioo 0 d) (z : α) :
-∃ c' : α, ∃ r' : ℝ≥0, ∃ d'' : ℝ,
-  closedBall c' r' ⊆ closedBall c r ∧
-  z ∉ closedBall c' r' ∧
-  diam (closedBall c' r') = d''
+(r₁ : ℝ≥0) (hr : r₁ ∈ Set.Ioc 0 r₀)
+--
+(z : α) :
+∃ c₁ : α,
+  closedBall c₁ r₁ ⊆ closedBall c₀ r₀ ∧
+  z ∉ closedBall c₁ r₁
   := by
-  replace hdpm := hdpm.is_dense c ⟨r, by linarith⟩
 
   sorry
+
+#check PadicComplex
