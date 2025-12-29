9ed4a825e998759ee752b570ee48a5c308b6c67b
YijunYuan <941201yuan@gmail.com>
2025-12-29 13:57:30 +0800

    feat: enhance spherically complete space definitions and properties

--- SphericalCompleteness/Quotient.lean
+++ SphericalCompleteness/Quotient.lean
@@ -168,28 +168,88 @@ private lemma hhh (𝕜 : Type u_1) [inst : NontriviallyNormedField 𝕜]
   simp only [QuotientAddGroup.mk'_apply, Submodule.mem_toAddSubgroup, hxF,
     QuotientAddGroup.mk_add_of_mem, Quotient.out_eq, true_and]
   refine lt_of_eq_of_lt ?_ hx
-  rw [(by grind : (unp1.out + x) - lun = unp1.out - lun + x)]
+  rw [(by grind only : (unp1.out + x) - lun = unp1.out - lun + x)]
 
-noncomputable def hhhh (𝕜 : Type u_1) [inst : NontriviallyNormedField 𝕜] [scsk : SphericallyCompleteSpace 𝕜]
+private noncomputable def hhhh (𝕜 : Type u_1) [inst : NontriviallyNormedField 𝕜]
 {E : Type u_2} [NormedAddCommGroup E]
   [NormedSpace 𝕜 E] [IsUltrametricDist E]
-  {F : Submodule 𝕜 E} [IsClosed (F : Set E)] ⦃c : ℕ → E ⧸ F⦄
+  {F : Submodule 𝕜 E} ⦃c : ℕ → E ⧸ F⦄
   ⦃r : ℕ → NNReal⦄ (hr : StrictAnti r) (hanti : Antitone fun i ↦ closedBall (c i) ↑(r i)) :
-  ℕ → E := fun n =>
+  (t : ℕ) → {x : E // (↑x : E ⧸ F.toAddSubgroup) = c t} := fun n =>
   match n with
-  | 0 => (c 0).out
-  | 1 => (c 1).out
-  | n + 2 => by
-    sorry
+  | 0 => ⟨(c 0).out, by simp only [Quotient.out_eq]⟩
+  | 1 => ⟨(c 1).out, by simp only [Quotient.out_eq]⟩
+  | m + 2 => by
+    have := hhh 𝕜 (c (m + 1)) (r (m + 1)) (c (m + 2)) (by
+      specialize hanti (Nat.le_succ (m+1))
+      refine hanti ?_
+      simp only [Nat.succ_eq_add_one, mem_closedBall, dist_self,
+        NNReal.zero_le_coe]) (hhhh 𝕜 hr hanti (m + 1)).val (by
+      simp only [QuotientAddGroup.mk'_apply, (hhhh 𝕜 hr hanti (m + 1)).prop]
+    ) (r m) (hr <| lt_add_one m)
+    exact ⟨this.choose, this.choose_spec.1⟩
+
+private lemma hhhh_prop (𝕜 : Type u_1) [inst : NontriviallyNormedField 𝕜]
+{E : Type u_2} [NormedAddCommGroup E]
+  [NormedSpace 𝕜 E] [IsUltrametricDist E]
+  {F : Submodule 𝕜 E} ⦃c : ℕ → E ⧸ F⦄
+  ⦃r : ℕ → NNReal⦄ (hr : StrictAnti r) (hanti : Antitone fun i ↦ closedBall (c i) ↑(r i)) :
+  ∀ i' : ℕ, ‖(hhhh 𝕜 hr hanti (i'+ 2)).val - (hhhh 𝕜 hr hanti (i' + 1)).val‖ < ↑(r i') := by
+  intro i'
+  simp only [hhhh, QuotientAddGroup.mk'_apply]
+  exact (hhh 𝕜 (c (i' + 1)) (r (i' + 1)) (c (i' + 2)) (by
+      specialize hanti (Nat.le_succ (i'+1))
+      refine hanti ?_
+      simp only [Nat.succ_eq_add_one, mem_closedBall, dist_self,
+        NNReal.zero_le_coe]) (hhhh 𝕜 hr hanti (i' + 1)).val (by
+      simp only [QuotientAddGroup.mk'_apply, (hhhh 𝕜 hr hanti (i' + 1)).prop]
+    ) (r i') (hr <| lt_add_one i')).choose_spec.2
 
 theorem Quotient.sphericallyCompleteSpace
-(𝕜 : Type*) [NontriviallyNormedField 𝕜] [scsk : SphericallyCompleteSpace 𝕜]
-{E : Type u_2} [NormedAddCommGroup E] [NormedSpace 𝕜 E] [IsUltrametricDist E]
-{F : Submodule 𝕜 E} [IsClosed (F : Set E)] :
+(𝕜 : Type*) [NontriviallyNormedField 𝕜]
+{E : Type u_2} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+[iud : IsUltrametricDist E] [scsk : SphericallyCompleteSpace E]
+{F : Submodule 𝕜 E} :
 SphericallyCompleteSpace (E ⧸ F) := by
   rw [sphericallyComplete_iff']
   intro c r hr hanti
-
-  sorry
+  let lc : ℕ → E := fun n => (hhhh 𝕜 hr hanti (n + 2)).val
+  let lr : ℕ → NNReal := fun n => r (n + 1)
+  have := @scsk.isSphericallyComplete (fun n => (hhhh 𝕜 hr hanti (n + 2)).val)
+    (fun n => r (n + 1)) (by
+    refine antitone_nat_of_succ_le fun n => ?_
+    intro z hz
+    simp only [mem_closedBall] at *
+    have := iud.dist_triangle_max z ↑(hhhh 𝕜 hr hanti (n + 1 + 2)) ↑(hhhh 𝕜 hr hanti (n + 2))
+    refine le_trans this <| sup_le_iff.2 ⟨le_trans hz <| le_of_lt <| hr <| lt_add_one _, ?_⟩
+    simp only [dist_eq_norm, le_of_lt <| hhhh_prop 𝕜 hr hanti (n + 1)]
+    )
+  simp only [Set.nonempty_iInter] at this
+  rcases this with ⟨w, hw⟩
+  use (QuotientAddGroup.mk' F.toAddSubgroup) w
+  simp only [QuotientAddGroup.mk'_apply, Set.mem_iInter, mem_closedBall]
+  intro i
+  have := (inferInstance : IsUltrametricDist (E ⧸ F)).dist_triangle_max
+    (↑w : E ⧸ F.toAddSubgroup) (c (i + 2)) (c i)
+  refine le_trans this <| sup_le_iff.2 ⟨?_, ?_⟩
+  · specialize hw i
+    simp only [mem_closedBall, dist_eq_norm] at hw
+    rw [← (hhhh 𝕜 hr hanti (i + 2)).prop, dist_eq_norm,
+    (by rfl : (↑w - ↑↑(hhhh 𝕜 hr hanti (i + 2)) : E ⧸ F.toAddSubgroup) =
+      ((QuotientAddGroup.mk' F.toAddSubgroup)) (w - ↑(hhhh 𝕜 hr hanti (i + 2)))),
+      quotient_norm_mk_eq F.toAddSubgroup]
+    replace := csInf_le (by
+      use 0
+      simp only [lowerBounds, Submodule.coe_toAddSubgroup, Set.mem_image, SetLike.mem_coe,
+        forall_exists_index, and_imp, forall_apply_eq_imp_iff₂, Set.mem_setOf_eq, norm_nonneg,
+        implies_true]
+      ) (by
+      use 0
+      simp only [Submodule.coe_toAddSubgroup, SetLike.mem_coe, zero_mem, add_zero, and_self]
+      : ‖w - ↑(hhhh 𝕜 hr hanti (i + 2))‖ ∈
+      ((fun x ↦ ‖w - ↑(hhhh 𝕜 hr hanti (i + 2)) + x‖) '' ↑F.toAddSubgroup))
+    exact le_trans (le_trans this hw) <| le_of_lt <| hr <| lt_add_one i
+  · refine (hanti <| Nat.le_add_right i 2) ?_
+    simp only [mem_closedBall, dist_self, NNReal.zero_le_coe]
 
 end SphericallyCompleteSpace
