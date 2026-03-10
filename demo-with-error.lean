ca1d897d67327b9d6567edd598a4efeb7f0457ae
YijunYuan <941201yuan@gmail.com>
2026-03-10 10:22:48 +0800

    feat: add miscellaneous code snippets illustrating norm inconsistencies in ℂ_[p]

---
+++ demo-with-error.lean
@@ -0,0 +1,22 @@
+import Mathlib.NumberTheory.Padics.Complex
+import Mathlib.Analysis.Normed.Group.Ultra
+import SphericalCompleteness.Dense
+
+/-
+# Miscellaneous code snippets (Not a part of the main library)
+This file contains some code snippets that appear in the paper. It is not intended to be a part of
+the main library, and it `concludes an error intendedly` to illustrate the inconsistency of the
+norms on `ℂ_[p]`.
+-/
+
+variable (p : ℕ) [hp : Fact (Nat.Prime p)]
+
+-- Embedding the algebraic closure of `ℚ` into the algebraic closure of `ℚ_[p]` by the universal
+-- property of algebraic closures.
+#check (Set.range <| @IsAlgClosed.lift (PadicAlgCl p) _ _ ℚ _ _ (AlgebraicClosure ℚ) _ _ _ _ _ _ _)
+
+open SphericallyCompleteSpace
+open IsUltrametricDist
+-- This will cause an error due to the inconsistency of the norms on `ℂ_[p]`, which should be
+-- coincide.
+#check @not_sphericallyCompleteSpace_of_isSphericallyDense_separable_ultrametric (ℂ_[p]) inferInstance (isUltrametricDist_of_isNonarchimedean_norm (PadicAlgCl.isNonarchimedean p))
